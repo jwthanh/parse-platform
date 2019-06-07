3b06058a2ee4285811bd9f0b06a6d9f975490e8e
Thanh Vu <thanh@ahiho.com>
2019-06-08 00:30:12 +0700

    firebase

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 const http = require('http');
 const express = require('express');
 const ParseServer = require('parse-server').ParseServer;
-const firebaseAuthAdapter = require('parse-server-firebase-auth');
+const firebaseAuthAdapter = require('parse-server-firebase-auth-adapter');
 const path = require('path');
 
 global.FirebaseAdmin = require('firebase-admin');
